88be4a6ddedfd29b05db2ca77b467cd2a6e848a9
Bin_Laptop <ethan.ramdeen64@gmail.com>
2026-07-07 15:54:20 -0400

    it boots, next goal is to get a working terminal going

--- build.zig
+++ build.zig
@@ -1,39 +1,79 @@
+
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
-    const target = b.standardTargetOptions(.{});
+    var disabled = std.Target.Cpu.Feature.Set.empty;
+    var enabled  = std.Target.Cpu.Feature.Set.empty;
+    
+    disabled.addFeature(@intFromEnum(std.Target.x86.Feature.mmx));
+    disabled.addFeature(@intFromEnum(std.Target.x86.Feature.sse));
+    disabled.addFeature(@intFromEnum(std.Target.x86.Feature.sse2));
+    disabled.addFeature(@intFromEnum(std.Target.x86.Feature.sse3));
+    disabled.addFeature(@intFromEnum(std.Target.x86.Feature.ssse3));
+    disabled.addFeature(@intFromEnum(std.Target.x86.Feature.sse4_1));
+    disabled.addFeature(@intFromEnum(std.Target.x86.Feature.sse4_2));
+    disabled.addFeature(@intFromEnum(std.Target.x86.Feature.avx));
+    disabled.addFeature(@intFromEnum(std.Target.x86.Feature.avx2));
+        
+    enabled.addFeature(@intFromEnum(std.Target.x86.Feature.soft_float));
+    
+    const target = std.Target.Query {
+        .cpu_arch = .x86_64,
+        .os_tag = .freestanding,
+        .abi = .none,
+        .cpu_features_add = enabled,
+        .cpu_features_sub = disabled,
+    };
+
     const optimize = b.standardOptimizeOption(.{});
 
-    const exe = b.addExecutable(.{
-        .name = "BLOS",
+    const kernel = b.addExecutable(.{
+        .name = "BlinkOS.elf",
         .root_module = b.createModule(.{
             .root_source_file = b.path("src/main.zig"),
-            .target = target,
+            .target = b.resolveTargetQuery(target),
             .optimize = optimize,
-            .imports = &.{},
+            .code_model = .kernel,
         }),
+        .use_llvm = true,
     });
 
-    b.installArtifact(exe);
+    kernel.setLinkerScript(b.path("linker-x86_64.ld"));
+    kernel.root_module.addAssemblyFile(b.path("src/asm/entry.s"));
 
-    const run_step = b.step("run", "Run the app");
+    b.installArtifact(kernel);
 
-    const run_cmd = b.addRunArtifact(exe);
+    
+    const iso_root = b.addWriteFiles();
+    _ = iso_root.addCopyFile(kernel.getEmittedBin(), "BlinkOS.elf");
+    _ = iso_root.addCopyFile(b.path("limine/limine-bios.sys"), "limine-bios.sys");
+    _ = iso_root.addCopyFile(b.path("limine/limine-bios-cd.bin"), "limine-bios-cd.bin");
+    _ = iso_root.addCopyFile(b.path("limine/limine.conf"), "limine.conf");
+
+    const xorriso = b.addSystemCommand(&.{
+        "xorriso",
+        "-as", "mkisofs",
+        "-b", "limine-bios-cd.bin",
+        "-no-emul-boot",
+        "-boot-load-size", "4",
+        "-boot-info-table", 
+    });
+
+    xorriso.addDirectoryArg(iso_root.getDirectory());
+    xorriso.addArg("-o");
+    const iso_out = xorriso.addOutputFileArg("bin/BlinkOS.iso");
+    const install_iso = b.addInstallFile(iso_out, "bin/BlinkOS.iso");
+
+    const make_iso_step = b.step("make-iso", "make an ISO image of the OS");
+    make_iso_step.dependOn(&install_iso.step);
+
+    const run_cmd = b.addSystemCommand(&.{
+        "qemu-system-x86_64",
+        "-cdrom"
+    });
+    
+    run_cmd.addFileArg(iso_out); 
+
+    const run_step = b.step("run", "run");
     run_step.dependOn(&run_cmd.step);
-
-    run_cmd.step.dependOn(b.getInstallStep());
-
-    if (b.args) |args| {
-        run_cmd.addArgs(args);
-    }
-
-    const exe_tests = b.addTest(.{
-        .root_module = exe.root_module,
-    });
-
-    const run_exe_tests = b.addRunArtifact(exe_tests);
-
-    const test_step = b.step("test", "Run tests");
-    test_step.dependOn(&run_exe_tests.step);
-
 }
--- src/main.zig
+++ src/main.zig
@@ -1,7 +1,27 @@
-const std = @import("std");
-const Io = std.Io;
+// multiboot header nonsense
+const MAGIC: u32 = 0xe85250d6;
+const ARCH : u32 = 0;
+const LEN  : u32 = @sizeOf(Multiboot2Header);
 
-pub fn main(init: std.process.Init) !void {
-    _ = init;
-    std.debug.print("Hello, World", .{});
+const Multiboot2Header = extern struct {
+    magic: u32 = MAGIC,
+    architecture: u32 = ARCH,
+    header_length: u32 = LEN,
+    checksum: u32,
+    
+    end_tag_type: u16 = 0,
+    end_tag_flags: u16 = 0,
+    end_tag_size: u32 = 8,
+};
+
+const magic_sum = MAGIC + ARCH + @as(u32, LEN);
+const calculated_checksum = ~magic_sum +% 1;
+
+export const multiboot_header: Multiboot2Header align(8) linksection(".multiboot") = .{
+    .checksum = calculated_checksum,
+};
+
+// the fun stuff
+export fn main() noreturn {
+    while (true) {}
 }
